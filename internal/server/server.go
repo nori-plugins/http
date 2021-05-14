8ea0ad988f11149c2212733a439c25411280fbf6
Anita Nabieva <bruteforce1414@gmail.com>
2021-05-14 11:51:39 +0500

    feat: implement Use(middlewares ...func(http.Handler) http.Handler) (#4)

--- internal/server/server.go
+++ internal/server/server.go
@@ -36,6 +36,7 @@ func (s *Server) HandleFunc(pattern string, h http.HandlerFunc) {
 func (s *Server) Method(method, pattern string, h http.Handler) {
 	s.router.Method(method, pattern, h)
 }
+
 func (s *Server) MethodFunc(method, pattern string, h http.HandlerFunc) {
 	s.router.MethodFunc(method, pattern, h)
 }
@@ -43,27 +44,39 @@ func (s *Server) MethodFunc(method, pattern string, h http.HandlerFunc) {
 func (s *Server) Connect(pattern string, h http.HandlerFunc) {
 	s.router.Connect(pattern, h)
 }
+
 func (s *Server) Delete(pattern string, h http.HandlerFunc) {
 	s.router.Delete(pattern, h)
 }
+
 func (s *Server) Get(pattern string, h http.HandlerFunc) {
 	s.router.Get(pattern, h)
 }
+
 func (s *Server) Head(pattern string, h http.HandlerFunc) {
 	s.router.Head(pattern, h)
 }
+
 func (s *Server) Options(pattern string, h http.HandlerFunc) {
 	s.router.Options(pattern, h)
 }
+
 func (s *Server) Patch(pattern string, h http.HandlerFunc) {
 	s.router.Patch(pattern, h)
 }
+
 func (s *Server) Post(pattern string, h http.HandlerFunc) {
 	s.router.Post(pattern, h)
 }
+
 func (s *Server) Put(pattern string, h http.HandlerFunc) {
 	s.router.Put(pattern, h)
 }
+
 func (s *Server) Trace(pattern string, h http.HandlerFunc) {
 	s.router.Trace(pattern, h)
+}
+
+func (s *Server) Use(middlewares ...func(http.Handler) http.Handler) {
+	s.router.Use(middlewares...)
 }
